dfb47cdc2784ce4602dd0b9200d42d10122d75f9
leejanghe <babybye777@gmail.com>
2022-03-30 22:07:29 +0900

    deps

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,13 +1,32 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 import {useState} from "react";
-
+import { useEffect } from "react";
 function App() {
 
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev)=> prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log('i run all the time')
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
+  console.log("serach keyword")
   return (
     <div>
+      <input
+      value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholder="search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
